a79b932aafad116cb8d7cac0561e735526328e0f
Jessica Melo <jessicamariademelo@hotmail.com>
2019-08-29 15:03:31 -0300

    no message

---
+++ tipo_String.py
@@ -0,0 +1,8 @@
+"""
+Tipo String
+
+Sempre que estiver entre aspas simples ou duplas ('4.2','2','jessica')
+"""
+nome = 'Jessica'
+print(nome)
+print(type(nome))
--- tipo_booleano.py
+++ tipo_booleano.py
@@ -1,4 +1,19 @@
 #True -> verdadeiro
 #False -> falso
 ativo = True
+logado = False
 print(ativo)
+print(not ativo)
+print(ativo or logado)
+"""
+ Ou (or)
+ Um deve ser verdadeiro
+
+ True or False
+ False or True
+ True or True
+ 
+ E (end)
+ Se tiver algume false é false tudo
+ se for tudo false é verdadeiro
+"""

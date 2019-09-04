8312ee4e74b2faca20098c9abc010d1ffe49bb76
Jessica Melo <jessicamariademelo@hotmail.com>
2019-09-04 18:31:55 -0300

    no message

--- listas.py
+++ listas.py
@@ -212,23 +212,93 @@ lista.append(33)
 lista.append(42)
 print(lista)
 
-
-"""
-
 #metodos não muito importantes mais tbm uteis
 #encontrar o indice  de um elemento na lista
 
-numeros = [5, 6, 7, 8, 9, 10]
+numeros = [5, 6, 7, 5, 8, 9, 10]
 #Em qual indice  da lista esta o valor 6?
 print(numeros.index(6))
 
 #Em qual indice  da lista esta o valor 6?
 print(numeros.index(9))
 
-Obs: caso não tenho esse elemento na lista 
+#print(numeros.index(19)) dará erro
+#Obs: caso não tenho esse elemento na lista, sera apresentado erro
+
+print(numeros.index(5))# retorna o indice do primeiro elemento encontrado
+
+#podemos fazer busca dentro de um range, ou seja, qual indice começar a buscar
+
+print(numeros.index(5, 1))#buscando a partir  do indice 1
+print(numeros.index(5, 2))#buscando a partir  do indice 2
+print(numeros.index(5, 3))#buscando a partir  do indice 3
+#print(numeros.index(5, 4))#buscando a partir  do indice 4
+#Obs: caso não tenho esse elemento na lista, sera apresentado erro
+
+#podemos fazer busca dentro de um range, inicio /fim
+print(numeros.index(8, 3, 6))# buscar indice  do valor 8 , entre os indices 3 e 6
 
 
+# Revisão do slicing
+#lista[inicio:fim:passo]
+#range(incio:fim:passo)
+
+#trabalhando com slaicing de lista com parametro 'inicio'
+
+lista = [1, 2, 3, 4]
+print(lista[::])# iniciando  no indice 1  e pegando todos os elementos restantes
+
+#trabalhando com slice de lista com o paraetro ' fim'
+#console
+#lista[1]
+#lsita[3:]
+#lista[-1:]
+
+print(lista[:2]) # começa em 0, pega até o indice 2 - 1
+print(lista[:4]) # começa em 0, pega até o indice 4 - 1
+print(lista[1:3]) # começa em 1, pega até o indice 3 - 1
+
+#trabalhando com slice de lista com o paraetro 'passo'
+
+print(lista[1::2]) #Começa em 1, vai até o final, de 2 em 2
+print(lista[::2]) #Começa em 0, vai até o final, de 2 em 2
+
+#invertendo valores da lista
+nomes = ['Jessica', 'Melo']
+nomes[0], nomes[1] = nomes[1], nomes[0]
+print(nomes)
+
+nomes = ['Jessica', 'Melo']
+nomes.reverse()
+print(nomes)
 
 
+#Soma*, valor maximo*, valor minimo*, tamanho
+#* Se valores forem inteiros ou flutuantes ( reais)
+
+lista = [1, 2, 3, 4, 5, 6]
+print(sum(lista)) #Soma
+print(max(lista)) #maximo valor
+print(min(lista)) #minimo valor
+print(len(lista)) #tamanho da lista
+
+#Transformar uma lista em tupla
+lista = [1, 2, 3, 4, 5, 6]
+print(lista)
+print(type(lista))
+
+tupla = tuple(lista) #tuplas usam parenteses
+print(tupla)
+print(type(tupla))
+
+#Desempacotamento de listas
+lista = [1, 2, 3,]
+num1, num2, num3 = lista
+print(num1)
+print(num2)
+print(num3)
+#se tivermos um numero diferente de elementos na lista ou variaveis para receber  os dados, teremos valueError
+
+"""
 
 
